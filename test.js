233e315f194788409ca3c2737df0b413e930783f
Sindre Sorhus <sindresorhus@gmail.com>
2016-04-07 14:15:47 +0700

    upgrade to latest AVA

--- test.js
+++ test.js
@@ -2,6 +2,6 @@ import test from 'ava';
 import x from './';
 
 test(t => {
-	t.same([9, 3, 24].sort(x.asc), [3, 9, 24]);
-	t.same([9, 3, 24].sort(x.desc), [24, 9, 3]);
+	t.deepEqual([9, 3, 24].sort(x.asc), [3, 9, 24]);
+	t.deepEqual([9, 3, 24].sort(x.desc), [24, 9, 3]);
 });
