17ece52fd5593cb51bf0b2526a70a2f6bc385e29
Sindre Sorhus <sindresorhus@gmail.com>
2020-05-29 15:45:16 +0800

    Update test.js

--- test.js
+++ test.js
@@ -2,6 +2,11 @@ import test from 'ava';
 import numberSort from '.';
 
 test('main', t => {
+	t.deepEqual([9, 3, 24].sort(numberSort.ascending), [3, 9, 24]);
+	t.deepEqual([9, 3, 24].sort(numberSort.descending), [24, 9, 3]);
+});
+
+test('supports NaN', t => {
 	t.deepEqual([9, -3, -Infinity, 24, NaN].sort(numberSort.ascending), [NaN, -Infinity, -3, 9, 24]);
 	t.deepEqual([9, -3, -Infinity, 24, NaN].sort(numberSort.descending), [24, 9, -3, -Infinity, NaN]);
 });
